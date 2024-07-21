47482fc2606f3b1a2d8c3524c72a39e9eaecb249
Anna <45910905+CatAnnaDev@users.noreply.github.com>
2024-07-21 23:40:18 +0200

    cat image random

--- src/main.rs
+++ src/main.rs
@@ -53,12 +53,10 @@ impl eframe::App for MyApp {
                 ui.columns(8, |columns| {
                     for cat in 0..self.cat_vec.len() {
                         columns[self.columns].group(|ui| {
-                            let img = self.cat_vec[cat].cat_image_byte.clone();
-                            println!("{img}");
-                            ui.add(egui::Image::new(format!("file://{img}")).rounding(10.0)).context_menu(|ui| {
+                            ui.add(egui::Image::new(format!("file://{}", self.cat_vec[cat].cat_image_byte.clone())).rounding(10.0)).context_menu(|ui| {
                                 if ui.add(Button::new("Feed")).clicked() { toast(&mut toasts, self.cat_vec[cat].feed()); }
                                 if ui.add(Button::new("Play")).clicked() { toast(&mut toasts, self.cat_vec[cat].play()); }
-                                if ui.add(Button::new("Sleep")).clicked() { toast(&mut toasts, self.cat_vec[cat].toggle_sleep()); }
+                                if ui.add(Button::new( format!("Sleep ({})", self.cat_vec[cat].sleep))).clicked() { toast(&mut toasts, self.cat_vec[cat].toggle_sleep()); }
                                 ui.menu_button("Mate with", |ui| {
                                     for mate_cat in 0..self.cat_vec.len() {
                                         if self.cat_vec[cat].gender.ne(&self.cat_vec[mate_cat].gender) {
